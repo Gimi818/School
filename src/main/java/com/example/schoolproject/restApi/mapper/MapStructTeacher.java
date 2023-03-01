0b0f86a8f4355570cef3df0f0ca001e0011ccfb8
Gimi818 <gimi8183@gmail.com>
2023-03-01 19:33:44 +0100

    difference between teacherDto / addTeacherDto in save Metod

---
+++ src/main/java/com/example/schoolproject/restApi/mapper/MapStructTeacher.java
@@ -0,0 +1,22 @@
+package com.example.schoolproject.restApi.mapper;
+
+import com.example.schoolproject.restApi.dto.AddTeacherDto;
+import com.example.schoolproject.restApi.dto.TeacherDto;
+import com.example.schoolproject.restApi.entity.Teacher;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MapStructTeacher {
+
+
+    MapStructTeacher MAPPER = Mappers.getMapper(MapStructTeacher.class);
+
+    TeacherDto entityMapToDto(Teacher teacher);
+
+
+    Teacher dtoMapToEntity(AddTeacherDto AddteacherDto);
+
+
+
+}
--- src/main/java/com/example/schoolproject/restApi/service/TeacherService.java
+++ src/main/java/com/example/schoolproject/restApi/service/TeacherService.java
@@ -3,8 +3,10 @@ package com.example.schoolproject.restApi.service;
 import com.example.schoolproject.restApi.dto.AddTeacherDto;
 import com.example.schoolproject.restApi.dto.TeacherDto;
 import com.example.schoolproject.restApi.entity.Teacher;
+import com.example.schoolproject.restApi.mapper.MapStructTeacher;
 import com.example.schoolproject.restApi.mapper.TeacherMapper;
 import com.example.schoolproject.restApi.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,19 +23,19 @@ public class TeacherService {
     private static final Logger logger = LoggerFactory.getLogger(TeacherService.class);
     private final TeacherRepository teacherRepository;
 
-    public final TeacherMapper mapper;
+    // public final TeacherMapper mapper;
 
 
-    public TeacherService(TeacherRepository teacherRepository, TeacherMapper mapper) {
+    public TeacherService(TeacherRepository teacherRepository) {
         this.teacherRepository = teacherRepository;
-        this.mapper = mapper;
     }
 
     public List<TeacherDto> findAll() {
         log.info("Get all students");
         log.debug("Get all students");
         return teacherRepository.findAll().stream()
-                .map(mapper::entityToDto)
+                //   .map(mapper::entityToDto)
+                .map(MapStructTeacher.MAPPER::entityMapToDto)
                 .collect(Collectors.toList());
     }
 
@@ -41,14 +43,15 @@ public class TeacherService {
         log.info("Get teacher by id");
         log.debug("Teacher id: " + id);
         Teacher teacher = teacherRepository.findById(id).orElseThrow();
-
-        return mapper.entityToDto(teacher);
+        return MapStructTeacher.MAPPER.entityMapToDto(teacher);
+        //   return mapper.entityToDto(teacher);
     }
 
     public Teacher saveTeacher(AddTeacherDto addTeacherDto) {
         log.info("Save teacher");
         log.debug("AddTeacherDto" + addTeacherDto);
-        Teacher teacher = teacherRepository.save(mapper.dtoToEntity(addTeacherDto));
+        //Teacher teacher = teacherRepository.save(mapper.dtoToEntity(addTeacherDto));
+        Teacher teacher = teacherRepository.save(MapStructTeacher.MAPPER.dtoMapToEntity(addTeacherDto));
         log.info("Teacher saved");
         return teacher;
 
@@ -59,8 +62,14 @@ public class TeacherService {
         log.info("Update Teacher");
         log.debug("Teacher id: " + id);
         Teacher teacher = teacherRepository.findById(id).orElseThrow();
-        teacherRepository.save(mapper.dtoToEntity(teacher, addTeacherDto));
+        teacher.setFirstName(addTeacherDto.getFirstName());
+        teacher.setLastName(addTeacherDto.getLastName());
+        teacher.setAge(addTeacherDto.getAge());
+        teacher.setYearsOfExperience(addTeacherDto.getYearsOfExperience());
+        //teacherRepository.save(mapper.dtoToEntity(teacher, addTeacherDto));
+       teacherRepository.save(MapStructTeacher.MAPPER.dtoMapToEntity( addTeacherDto));
         log.info("Teacher updated");
+
     }
 
     public void deleteTeacher(Long id) {
